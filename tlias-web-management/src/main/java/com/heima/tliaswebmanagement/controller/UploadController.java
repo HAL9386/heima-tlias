5eaf301231d5d1b468521add404753e3bccaaf24
smile <mad024039@gmail.com>
2025-04-22 22:28:58 +0800

    feat: 实现保存文件到本地

--- tlias-web-management/src/main/java/com/heima/tliaswebmanagement/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/heima/tliaswebmanagement/controller/UploadController.java
@@ -6,12 +6,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
 @Slf4j
 @RestController
 public class UploadController {
   @PostMapping("/upload")
-  public Result upload(String name, Integer age, MultipartFile file) {
+  public Result upload(String name, Integer age, MultipartFile file) throws IOException {
     log.info("文件上传：{}，{}，{}", name, age, file.getOriginalFilename());
+    // 保存文件到服务器
+    String originalFilename = file.getOriginalFilename();
+    String filenameExt = originalFilename.substring(originalFilename.lastIndexOf("."));
+    String newFilename = UUID.randomUUID().toString() + filenameExt;
+    String path = "D:/tmp/heima/tlias/images/";
+    makeDir(path);
+    file.transferTo(new File(path + newFilename));
     return Result.success();
+  }
+  private void makeDir(String dirPath) {
+    File dir = new File(dirPath);
+    if (!dir.exists()) {
+      dir.mkdirs();
+    }
   }
 }
