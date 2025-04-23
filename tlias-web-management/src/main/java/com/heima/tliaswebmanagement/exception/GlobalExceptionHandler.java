6e949259f55c4bca156f275849c4e7418492783e
smile <mad024039@gmail.com>
2025-04-23 20:15:52 +0800

    feat: 实现全局异常处理

---
+++ tlias-web-management/src/main/java/com/heima/tliaswebmanagement/exception/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.heima.tliaswebmanagement.exception;
+
+import com.heima.tliaswebmanagement.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+  @ExceptionHandler
+  public Result handleException(Exception e) {
+    log.error("全局异常处理器：", e);
+    return Result.error("对不起，操作失败");
+  }
+
+  @ExceptionHandler
+  public Result handleDuplicateKeyException(DuplicateKeyException e) {
+    log.error("全局异常处理器：", e);
+    String msg = e.getMessage();
+    int i = msg.indexOf("Duplicate entry");
+    String[] errMsg = msg.substring(i).split(" ");
+    return Result.error(errMsg[2] + "已存在");
+  }
+}
